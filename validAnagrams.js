fcbf52d88b8d66830a067d374e8feccbffc2bb87
Christina Son <christinason17@gmail.com>
2022-05-11 14:58:29 -0400

    updated validAnagrams solution

--- validAnagrams.js
+++ validAnagrams.js
@@ -11,23 +11,24 @@
 
 // tina's first attempt: brute force
 // time complexity: O(2n) ????
+// space complexity: O(n)
 var isAnagram = function (s, t) {
   // edge case: check if length of arguments are different
   if (s.length !== t.length) return false;
 
   // map t to an object with key value pairs
   const t_Obj = {};
-  t.split('').forEach(char => t_Obj[char] !== undefined ? t_Obj[char]++ : t_Obj[char] = 0);
+  t.split('').forEach(char => t_Obj[char] !== undefined ? t_Obj[char]++ : t_Obj[char] = 1);
   // console.log(t_Obj)
 
   // iterate through s and check if object contains key 
   for (let i = 0; i < s.length; i++) {
-    if (t_Obj[s[i]] > 0) t_Obj[s[i]]--;
-    else if (t_Obj[s[i]] === 0) delete t_Obj[s[i]];
-    // console.log(t_Obj)
+    if (t_Obj[s[i]] >= 0) t_Obj[s[i]]--;
+    // else if (t_Obj[s[i]] === 0) delete t_Obj[s[i]];
   }
-
-  return Object.keys(t_Obj).every(el => el === 0);
+  // console.log(t_Obj)
+  // return Object.keys(t_Obj).length === 0;
+  return Object.values(t_Obj).every(el => el === 0);
 }
 
 // const s = 'aaca'

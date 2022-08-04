7b4a2b77f0a675c8aea45be0c7b2464b5ee544fb
Christina Son <christinason@Christinas-MacBook-Pro.local>
2022-08-04 17:09:49 -0400

    add minimumMeetingRooms solution

--- two-heaps-pattern/findTheMedianOfANumberStream.js
+++ two-heaps-pattern/findTheMedianOfANumberStream.js
@@ -7,13 +7,20 @@ If the count of numbers inserted in the class is even, the median will be the av
 level: medium
 */
 
-const Heap = require('./collections/heap'); //http://www.collectionsjs.com
+/*
+The best data structure that comes to mind to find the smallest or largest number among a list of numbers is a Heap.
 
+We can store the first half of numbers (i.e., smallNumList) in a Max Heap. We should use a Max Heap as we are interested in knowing the largest number in the first half.
+We can store the second half of numbers (i.e., largeNumList) in a Min Heap, as we are interested in knowing the smallest number in the second half.
+Inserting a number in a heap will take O(logN), which is better than the brute force approach.
+At any time, the median of the current list of numbers can be calculated from the top element of the two heaps. */
+
+const Heap = require("./collections/heap"); //http://www.collectionsjs.com
 
 class MedianOfAStream {
   constructor() {
-    this.maxHeap = new Heap([], null, ((a, b) => a - b)); // containing first half of numbers
-    this.minHeap = new Heap([], null, ((a, b) => b - a)); // containing second half of numbers
+    this.maxHeap = new Heap([], null, (a, b) => a - b); // containing first half of numbers
+    this.minHeap = new Heap([], null, (a, b) => b - a); // containing second half of numbers
   }
 
   insert_num(num) {
@@ -43,7 +50,6 @@ class MedianOfAStream {
   }
 }
 
-
 const medianOfAStream = new MedianOfAStream();
 medianOfAStream.insert_num(3);
 medianOfAStream.insert_num(1);
@@ -52,3 +58,13 @@ medianOfAStream.insert_num(5);
 console.log(`The median is: ${medianOfAStream.find_median()}`);
 medianOfAStream.insert_num(4);
 console.log(`The median is: ${medianOfAStream.find_median()}`);
+
+/*
+Time complexity:
+The time complexity of the insertNum() will be O(logN), due to the insertion in the heap. T
+The time complexity of the findMedian() will be O(1) as we can find the median from the top elements of the heaps.
+
+Space complexity:
+The space complexity will be O(N) because, as at any time, we will be storing all the numbers.
+
+ */

a75647f8b70c29c01e87b219ca16b8ea4a1aa8f1
Christina Son <christinason17@gmail.com>
2022-09-08 17:45:17 -0400

    add linkedListCycle solution

---
+++ fast-and-slow-pointers-pattern/startOfLinkedListCycle.js
@@ -0,0 +1,34 @@
+/* 
+Given the head of a Singly LinkedList that contains a cycle, write a function to find the starting node of the cycle.
+
+level: medium
+*/
+
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+const find_cycle_start = function (head) {
+  // find the tail of linked list. where it's next value points to is the start of the cycle
+  
+  return head;
+};
+
+head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+head.next.next.next.next.next = new Node(6);
+
+head.next.next.next.next.next.next = head.next.next;
+console.log(`LinkedList cycle start: ${find_cycle_start(head).value}`);
+
+head.next.next.next.next.next.next = head.next.next.next;
+console.log(`LinkedList cycle start: ${find_cycle_start(head).value}`);
+
+head.next.next.next.next.next.next = head;
+console.log(`LinkedList cycle start: ${find_cycle_start(head).value}`);

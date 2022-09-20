3e1ac7ae12fe7a3a0cb1298405389a46dd6091c2
Christina Son <christinason17@gmail.com>
2022-09-20 15:26:25 -0400

    add solution for slidingWindowMedian clera

--- two-heaps-pattern/heaps.js
+++ two-heaps-pattern/heaps.js
@@ -2,73 +2,85 @@
 
 // left child: i * 2
 // right child: i * 2 + 1
-// parent: i / 2
+// parent: floor (i / 2)
 
 let MinHeap = function () {
   let heap = [null];
 
   this.insert = function (num) {
-    heap.push(num);
-    if (heap.length > 2) {
-      let idx = heap.length - 1;
-      while (heap[idx] < heap[Math.floor(idx / 2)]) {
-        if (idx >= 1) {
-          [heap[Math.floor(idx / 2)], heap[idx]] = [
-            heap[idx],
-            heap[Math.floor(idx / 2)],
-          ];
-          if (Math.floor(idx / 2) > 1) {
-            idx = Math.floor(idx / 2);
-          } else {
-            break;
-          }
-        }
+    /* Inserting the new node at the end of the heap array */
+    heap.push(node);
+
+    /* Finding the correct position for the new node */
+    if (heap.length > 1) {
+      let current = heap.length - 1;
+
+      /* Traversing up the parent node until the current node (current) is greater than the parent (current/2)*/
+      while (current > 1 && heap[Math.floor(current / 2)] > heap[current]) {
+        /* Swapping the two nodes by using the ES6 destructuring syntax*/
+        [heap[Math.floor(current / 2)], heap[current]] = [
+          heap[current],
+          heap[Math.floor(current / 2)],
+        ];
+        current = Math.floor(current / 2);
       }
     }
   };
 
   this.remove = function () {
-    let smallest = heap[1];
-    if (heap.length > 2) {
-      heap[1] = heap[heap.length - 1];
-      heap.splice(heap.length - 1);
-      if (heap.length == 3) {
-        if (heap[1] > heap[2]) {
-          [heap[1], heap[2]] = [heap[2], heap[1]];
+    /* Smallest element is at the index 1 in the heap array */
+    let smallest = this.heap[1];
+
+    /* When there are more than two elements in the array, we put the right most element at the first position
+      and start comparing nodes with the child nodes
+  */
+    if (this.heap.length > 2) {
+      this.heap[1] = this.heap[this.heap.length - 1];
+      this.heap.splice(this.heap.length - 1);
+
+      if (this.heap.length === 3) {
+        if (this.heap[1] > this.heap[2]) {
+          [this.heap[1], this.heap[2]] = [this.heap[2], this.heap[1]];
         }
         return smallest;
       }
-      let i = 1;
-      let left = 2 * i;
-      let right = 2 * i + 1;
-      while (heap[i] >= heap[left] || heap[i] >= heap[right]) {
-        if (heap[left] < heap[right]) {
-          [heap[i], heap[left]] = [heap[left], heap[i]];
-          i = 2 * i;
+
+      let current = 1;
+      let leftChildIndex = current * 2;
+      let rightChildIndex = current * 2 + 1;
+
+      while (
+        this.heap[leftChildIndex] &&
+        this.heap[rightChildIndex] &&
+        (this.heap[current] > this.heap[leftChildIndex] ||
+          this.heap[current] > this.heap[rightChildIndex])
+      ) {
+        if (this.heap[leftChildIndex] < this.heap[rightChildIndex]) {
+          [this.heap[current], this.heap[leftChildIndex]] = [
+            this.heap[leftChildIndex],
+            this.heap[current],
+          ];
+          current = leftChildIndex;
         } else {
-          [heap[i], heap[right]] = [heap[right], heap[i]];
-          i = 2 * i + 1;
+          [this.heap[current], this.heap[rightChildIndex]] = [
+            this.heap[rightChildIndex],
+            this.heap[current],
+          ];
+          current = rightChildIndex;
         }
-        left = 2 * i;
-        right = 2 * i + 1;
-        if (heap[left] == undefined || heap[right] == undefined) {
-          break;
-        }
+
+        leftChildIndex = current * 2;
+        rightChildIndex = current * 2 + 1;
       }
-    } else if (heap.length == 2) {
-      heap.splice(1, 1);
+    } else if (this.heap.length === 2) {
+
+    /* If there are only two elements in the array, we directly splice out the first element */
+      this.heap.splice(1, 1);
     } else {
       return null;
     }
-    return smallest;
-  };
 
-  this.sort = function () {
-    let result = new Array();
-    while (heap.length > 1) {
-      result.push(this.remove());
-    }
-    return result;
+    return smallest;
   };
 };
 

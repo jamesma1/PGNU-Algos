7accd29c21a5737321bddf617d1b63d12af2fda1
Christina Son <christinason17@gmail.com>
2022-09-29 16:06:12 -0400

    add findTheCorruptPair solution

---
+++ cyclic-sort-pattern/findTheCorruptPair.js
@@ -0,0 +1,36 @@
+/* 
+We are given an unsorted array containing ‘n’ numbers taken from the range 1 to ‘n’. 
+The array originally contained all the numbers from 1 to ‘n’, but due to a data error, 
+one of the numbers got duplicated which also resulted in one number going missing. Find both these numbers.
+
+level: easy
+*/
+
+function find_corrupt_numbers(nums) {
+  let i = 0;
+  while (i < nums.length) {
+    const j = nums[i] - 1;
+    if (nums[i] !== nums[j]) {
+      [nums[i], nums[j]] = [nums[j], nums[i]]; // swap
+    } else {
+      i += 1;
+    }
+  }
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] !== i + 1) {
+      return [nums[i], i + 1];
+    }
+  }
+  return [-1, -1];
+}
+
+console.log(find_corrupt_numbers([3, 1, 2, 5, 2]));
+console.log(find_corrupt_numbers([3, 1, 2, 3, 6, 4]));
+
+/* 
+Time complexity:
+The time complexity of the above algorithm is O(n).
+
+Space complexity:
+Ignoring the space required for storing the duplicates, the algorithm runs in constant space O(1).
+*/
